72ef4658832731265e9e373c8c6acd2d0b3fbc4e
Julien Breton <julienbreton@hotmail.fr>
2019-03-28 22:02:57 +0100

    feature(upload): add a volume to share files
    Add a volume in the docker-compose.yaml to share file between the host and the containers.
    It is usefull to upload files in a Web app.

--- src/test/java/julien/selenium/SeleniumTest.java
+++ src/test/java/julien/selenium/SeleniumTest.java
@@ -3,12 +3,17 @@ package julien.selenium;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
 import org.testng.ITestContext;
 
 public class SeleniumTest {
@@ -49,5 +54,24 @@ public class SeleniumTest {
 
 		driver.get("https://www.google.fr");
 		Assert.assertEquals(driver.getTitle(), "Google");
+	}
+	
+	@Test
+	public void uploadFile() {
+		
+		//In this test we check if we can access to the file shared between the host and the container.
+		//See the volume with /home/julien/test:/home/selenium/upload in the docker-compose.yaml.
+
+		driver.get("http://demo.guru99.com/test/upload/");
+		
+		//Upload the file
+		WebElement uploadElement = driver.findElement(By.id("uploadfile_0"));
+		uploadElement.sendKeys("/home/selenium/upload/test.txt");
+	    driver.findElement(By.id("terms")).click();
+	    driver.findElement(By.name("send")).click();
+	    
+	    //Check if the file is uploaded
+	    WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("#res > center"), "1 file\nhas been successfully uploaded.")); 
 	}
 }
