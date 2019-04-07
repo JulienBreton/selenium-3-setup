f648d28a1a60804bbb9d9da35854659a3a9e1364
Julien Breton <julienbreton@hotmail.fr>
2019-04-07 17:33:17 +0200

    feat: add the ability to take screenshot
    Add the ability to take screenshot when a test failed.

--- src/test/java/julien/selenium/SeleniumTest.java
+++ src/test/java/julien/selenium/SeleniumTest.java
@@ -1,53 +1,17 @@
 package julien.selenium;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import tools.ScreenshotTestRule;
+import tools.Browser;
 
-import org.testng.ITestContext;
-
-public class SeleniumTest {
-
-	WebDriver driver;
-
-	@BeforeTest
-	public void setUp(ITestContext context) throws MalformedURLException {
-
-		String sBrowser = context.getCurrentXmlTest().getParameter("selenium.browser");
-		String sRemoteAddress = context.getCurrentXmlTest().getParameter("selenium.remoteAddress");
-
-		if (sBrowser.equalsIgnoreCase("firefox")) {
-
-			//The geckodriver is in the same folder than the selenium-server-standalone 
-			driver = new RemoteWebDriver(new URL(sRemoteAddress), new FirefoxOptions());
-		}
-		else if (sBrowser.equalsIgnoreCase("chrome")) {
-
-			//The chromedriver is in the same folder than the selenium-server-standalone 
-			driver = new RemoteWebDriver(new URL(sRemoteAddress), new ChromeOptions());
-		}
-		else
-		{
-			throw new IllegalArgumentException("[ERROR] The browser is not defined or unknowed. Actual browser : -"+sBrowser+"-");
-		}
-
-	}
-
-	@AfterTest
-	public void afterTest() {
-
-		driver.quit();
-	}
+@Listeners({ScreenshotTestRule.class})
+public class SeleniumTest extends Browser{
 
 	@Test(invocationCount = 1)
 	public void sampleTest() {
